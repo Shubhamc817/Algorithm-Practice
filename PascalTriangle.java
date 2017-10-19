067fc9445a1b1c0b721e3e32b9153d863f01ace6
Shubhamc817 <shubhamc817@gmail.com>
2017-10-19 10:17:17 +0530

    Update PascalTriangle.java

--- PascalTriangle.java
+++ PascalTriangle.java
@@ -1,16 +1,26 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
+/*
+for n=7, pascal triangle would look like
+1 
+1 1 
+1 2 1 
+1 3 3 1 
+1 4 6 4 1 
+1 5 10 10 5 1 
+1 6 15 20 15 6 1 
+*/
 class PascalTriangle
  {
 	public static void main (String[] args)
 	 {
-	 //code
+	 
 	 Scanner sc=new Scanner(System.in);
-	 int T=sc.nextInt();
+	 int T=sc.nextInt();//denotes the number of test cases
 	 while(T-->0)
 	 {
-	  int n=sc.nextInt();
+	  int n=sc.nextInt(); 
 	  int ar[][]=new int[n][n];
 	  for(int line=0;line<n;line++)
 	  {
@@ -22,7 +32,7 @@ class PascalTriangle
 	          ar[line][i]=ar[line-1][i-1]+ar[line-1][i];
 	          System.out.print(ar[line][i]+" ");
 	      }
-
+          System.out.println();
 	      
 	  }
 	  System.out.println();
